ea95c864ea6b60e1394e05eb32b91e79636d2490
tkoizumi <takayuki.koizumi@gmail.com>
2023-11-29 12:31:20 -0500

    initial api setup

---
+++ backend/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette import status
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    return {"message": "oauth api home page"}
