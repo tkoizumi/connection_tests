0687915873db8e4c1abee1b8f353cb7bae7b322d
tkoizumi <takayuki.koizumi@gmail.com>
2023-11-29 14:19:58 -0500

    add basic auth

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+import secrets
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
 from starlette.middleware.cors import CORSMiddleware
 from starlette import status
 
 app = FastAPI()
+security = HTTPBasic()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,6 +19,33 @@ app.add_middleware(
 )
 
 
+def get_current_username(
+    credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+):
+    current_username_bytes = credentials.username.encode("utf8")
+    correct_username_bytes = b"cat"
+    is_correct_username = secrets.compare_digest(
+        current_username_bytes, correct_username_bytes
+    )
+    current_password_bytes = credentials.password.encode("utf8")
+    correct_password_bytes = b"meow"
+    is_correct_password = secrets.compare_digest(
+        current_password_bytes, correct_password_bytes
+    )
+    if not (is_correct_username and is_correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
 @app.get("/")
 def root():
-    return {"message": "You have accessed a very very private machine - Taka"}
+    return {"message": "You have accessed a private machine - Taka"}
+
+
+@app.get("/basic")
+def root(username: Annotated[str, Depends(get_current_username)]):
+    return {"message": "You have accessed a very private machine using basic auth - Taka"}
